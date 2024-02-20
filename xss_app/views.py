8726db7171df1902224cb87b124ffa0ae5c349b6
Kryštof Bezděk <krystof.bezdek@gmail.com>
2024-02-20 15:53:26 +0100

    Adding functionality to unlock exercise 2 after finishing 1

--- xss_app/views.py
+++ xss_app/views.py
@@ -1,23 +1,42 @@
-from django.db import connection
-# from unittest import loader
-
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-from django.core.mail import send_mail
-# from django.db.models import Q
 
 from .models import Blog
+from . import globals
 
 
 # Define model by queryset or model =
 class IndexView(generic.ListView):
-    template_name = "xss_app/index.html" # Specify like this or it uses default like <app name>/<model name>_detail.html
+    template_name = "xss_app/index.html"
     context_object_name = "blog_post_list"
+
+    def __init__(self):
+        self.object_list = None
+        self.part1Completed = globals.PART1_COMPLETED
 
     def get_queryset(self):
         return Blog.objects.order_by("-pub_date")[:10]
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['part1Completed'] = self.part1Completed
+        return context
+
+    def post(self, request, *args, **kwargs):
+        user_input_cookie = request.POST.get('cookieInput')
+        real_cookie = request.POST.get('realCookie')
+        csrf_value = real_cookie.split('=')[1]
+
+        if user_input_cookie == real_cookie or user_input_cookie == csrf_value:
+            globals.PART1_COMPLETED = True
+        else:
+            globals.PART1_COMPLETED = False
+
+        self.part1Completed = globals.PART1_COMPLETED
+        self.object_list = self.get_queryset()
+        return render(request, "xss_app/index.html", self.get_context_data(**kwargs))
 
 
 class DetailView(generic.DetailView):
@@ -25,33 +44,14 @@ class DetailView(generic.DetailView):
     template_name = "xss_app/detail.html"
 
 
-# http://127.0.0.1:8000/xss_app/search/?q=%3Cscript%3Edocument.addEventListener(%27DOMContentLoaded%27,%20(event)%20=%3E%20{%20window.location.href%20=%20%27https://www.google.com%27;%20});%3C/script%3E
 class SearchResultsView(generic.ListView):
     model = Blog
     template_name = "xss_app/search.html"
 
     def get_queryset(self):
-        query = self.request.GET.get("q") # supply anything' OR 1 = 1 --;
-
-        # SAFE
+        query = self.request.GET.get("q")
         param = f'%{query}%'
         object_list = Blog.objects.raw("SELECT * FROM xss_app_blog WHERE headline LIKE %s", [param])
-
-        # UNSAFE - SQL INJECTION
-        # with connection.cursor() as cursor:
-        #     cursor.execute("SELECT * FROM xss_app_blog WHERE headline LIKE '%" + query + "%'")
-        #     rows = cursor.fetchall()
-        #
-        # results = []
-        # for row in rows:
-        #     row_data = {
-        #         "id": row[0],
-        #         "headline": row[1],
-        #         "content": row[2],
-        #         "author": row[3],
-        #         "pub_date": row[4]
-        #     }
-        #     results.append(row_data)
         return object_list
 
     def get_context_data(self, **kwargs):
@@ -59,7 +59,7 @@ class SearchResultsView(generic.ListView):
         context['search_query'] = self.request.GET.get("q")
         return context
 
-# <script>function callfunc() {fetch('http://127.0.0.1:8000/xss_app/create?headline=HACKED&content=' + document.cookie).then(response => response.text()).then(data => {console.log(data);}).catch(error => {console.error('Error:', error);});}</script><div onmouseover="callfunc()">You have been hacked</div>
+
 class BlogPostCreateView(generic.View):
     def get(self, request, *args, **kwargs):
         headline = request.GET.get('headline')
