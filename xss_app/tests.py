95d6893165e5b65d9a04ef9c8f3c6dc650d4c2ba
Kryštof Bezděk <krystof.bezdek@gmail.com>
2024-02-20 18:00:50 +0100

    Using regex to check for basic xss attacks and changing dynamic content based on results

--- xss_app/tests.py
+++ xss_app/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+examples = [
+    '<script>alert("XSS")</script>',
+    '<img src="x" onerror="alert(\'XSS\')" />',
+    '<a href="javascript:alert(\'XSS\')">click me</a>',
+    '<?php echo "Hello world"; ?>',
+]
--- xss_app/views.py
+++ xss_app/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -6,22 +8,25 @@ from django.views import generic
 from .models import Blog
 from . import globals
 
+xss_pattern = re.compile(r'(?:<script.*?>.*?</script>|<.*?javascript:.*?>|<.*?on\w+.*?=.*?>|<\?.*?\?>)', re.IGNORECASE)
 
-# Define model by queryset or model =
+
 class IndexView(generic.ListView):
     template_name = "xss_app/index.html"
     context_object_name = "blog_post_list"
 
     def __init__(self):
         self.object_list = None
-        self.part1Completed = globals.PART1_COMPLETED
+        self.part1_completed = globals.PART1_COMPLETED
+        self.performed_reflected_xss = globals.PERFORMED_REFLECTED_ATTACK
 
     def get_queryset(self):
         return Blog.objects.order_by("-pub_date")[:10]
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['part1Completed'] = self.part1Completed
+        context['part1_completed'] = self.part1_completed
+        context['performed_reflected_xss'] = self.performed_reflected_xss
         return context
 
     def post(self, request, *args, **kwargs):
@@ -29,12 +34,12 @@ class IndexView(generic.ListView):
         real_cookie = request.POST.get('realCookie')
         csrf_value = real_cookie.split('=')[1]
 
-        if user_input_cookie == real_cookie or user_input_cookie == csrf_value:
+        if user_input_cookie == real_cookie or user_input_cookie == csrf_value and self.performed_reflected_xss:
             globals.PART1_COMPLETED = True
         else:
             globals.PART1_COMPLETED = False
 
-        self.part1Completed = globals.PART1_COMPLETED
+        self.part1_completed = globals.PART1_COMPLETED
         self.object_list = self.get_queryset()
         return render(request, "xss_app/index.html", self.get_context_data(**kwargs))
 
@@ -43,6 +48,25 @@ class DetailView(generic.DetailView):
     model = Blog
     template_name = "xss_app/detail.html"
 
+    def get_object(self):
+        blog = get_object_or_404(Blog, pk=self.kwargs['pk'])
+        return blog
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        blog = self.get_object()
+        context['referer'] = self.request.META.get('HTTP_REFERER')
+
+        if xss_pattern.search(blog.headline):
+            context['success'] = True
+        else:
+            context['success'] = False
+
+        if not xss_pattern.search(blog.headline) and not xss_pattern.search(blog.content):
+            context['neutral'] = True
+
+        return context
+
 
 class SearchResultsView(generic.ListView):
     model = Blog
@@ -50,6 +74,8 @@ class SearchResultsView(generic.ListView):
 
     def get_queryset(self):
         query = self.request.GET.get("q")
+        if xss_pattern.search(query):
+            globals.PERFORMED_REFLECTED_ATTACK = True
         param = f'%{query}%'
         object_list = Blog.objects.raw("SELECT * FROM xss_app_blog WHERE headline LIKE %s", [param])
         return object_list
@@ -66,9 +92,9 @@ class BlogPostCreateView(generic.View):
         content = request.GET.get('content')
 
         if headline and content:
-            if ("<script>" in content or "</script>" in content) and ("<script>" not in headline or "</script>" not in headline):
+            if xss_pattern.search(content) and not xss_pattern.search(headline):
                 headline = "Ha! The rendering of blog post content on this wall is safe!"
-            if "<script>" in headline or "</script>" in headline:
+            if xss_pattern.search(headline):
                 content = "Ha! The rendering of blog post headlines on this wall is safe!"
             author = request.user.username if request.user.is_authenticated else "Anonymous"
             Blog.objects.create(headline=headline, content=content, author=author)
